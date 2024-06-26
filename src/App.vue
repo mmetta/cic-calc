f78a9628638c1e2b00b7d6aca9ef01de8bcec223
mmetta <42844751+mmetta@users.noreply.github.com>
2024-06-26 08:01:43 -0300

    implementa service-worker

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,38 @@
     <v-main>
       <router-view/>
     </v-main>
+    <v-snackbar multi-line centered :value="updateExists" :timeout="-1" color="indigo">
+      Novo conteúdo disponível para o APP
+      <v-btn text @click="refreshApp">
+        <v-icon class="mr-2">mdi-autorenew</v-icon>
+        atualizar
+      </v-btn>
+    </v-snackbar>
   </v-app>
 </template>
 
 <script>
+import update from "./mixins/update";
 
 export default {
   name: 'App',
-
+  mixins: [update],
   data: () => ({
-    //
+    drawer: false,
+    group: null,
+    deferredPrompt: null,
   }),
+  created() {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+    });
+  },
+  methods: {
+    async install() {
+      this.deferredPrompt.prompt();
+    },
+  },
 };
 </script>
